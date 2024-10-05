9f26e254381dbf0f51aa567c79673339ad1d4e7d
Krishant Tanti <66065803+Djkt2005@users.noreply.github.com>
2024-10-05 14:56:26 +0530

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,18 @@
+def fibonacci_array(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib_array = [0, 1]
+    for i in range(2, n):
+        fib_array.append(fib_array[-1] + fib_array[-2])
+    
+    return fib_array
+
+# Example usage:
+n = 10
+print(fibonacci_array(n))
+
